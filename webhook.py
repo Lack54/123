be029a24da4c2332d6a4cf05a3e3bbc3c449e7e7
Lack <118399851+Lack54@users.noreply.github.com>
2023-05-02 20:01:27 -0400

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -2,7 +2,7 @@ from requests.exceptions import Timeout
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import time
 def main(title, discription, color):
-    webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/1062557937589047366/lIF21r3W4SIs56uq0siij1JA9xFFemOYgAp_MyG6O9qETg2DdLXv1jeKqbjkftSaDJTa", timeout=10)
+    webhook = DiscordWebhook(url="webhook", timeout=10)
     embed = DiscordEmbed(title=title, description=discription, color=color)
 
 
